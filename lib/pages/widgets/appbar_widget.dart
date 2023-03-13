d1b860009c232fe9d41f91244e6ca8d86c0655fa
Akbar <akbar358888@gmail.com>
2023-03-13 13:05:53 +0500

    i

--- lib/pages/widgets/appbar_widget.dart
+++ lib/pages/widgets/appbar_widget.dart
@@ -9,24 +9,35 @@ class AppBarWidget extends StatelessWidget with PreferredSizeWidget {
   const AppBarWidget({Key? key}) : super(key: key);
 
   @override
-  Size get preferredSize => throw UnimplementedError();
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+        elevation: 0,
         title: Container(
-      width: MediaQuery.of(context).size.width,
-      height: 40,
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(5)),
-      child: TextField(
-          controller: _seachController,
-          decoration: InputDecoration(
-              labelText: S.of(context).seach_product,
-              hintText: S.of(context).seach_product,
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(25.0))))),
-    ));
+          width: MediaQuery.of(context).size.width,
+          height: 50,
+          // color: Colors.white54,
+          alignment: Alignment.center,
+          child: TextField(
+              controller: _seachController,
+              textAlign: TextAlign.justify,
+              decoration: InputDecoration(
+                  hintText: S.of(context).seach_product,
+                  prefixIcon: Icon(Icons.search),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 5, color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 5, color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 5, color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))))),
+        ));
   }
 }
