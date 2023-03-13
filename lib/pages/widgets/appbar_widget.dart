9e32212ce2fdabddb34c2e885214f7baa8e92fc7
Akbar <akbar358888@gmail.com>
2023-03-13 21:17:42 +0500

    i

--- lib/pages/widgets/appbar_widget.dart
+++ lib/pages/widgets/appbar_widget.dart
@@ -5,9 +5,11 @@ import 'package:get/get_core/src/get_main.dart';
 
 import '../../controller/Controller.dart';
 import '../../generated/l10n.dart';
+import '../../models/catalogs/Catalog.dart';
 
 TextEditingController _seachController = TextEditingController();
 final Controller _controller = Get.find();
+List<Catalog> finalList = [];
 
 class AppBarWidget extends StatelessWidget with PreferredSizeWidget {
   const AppBarWidget({Key? key}) : super(key: key);
@@ -20,10 +22,7 @@ class AppBarWidget extends StatelessWidget with PreferredSizeWidget {
     return AppBar(
         elevation: 0,
         title: Container(
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
+          width: MediaQuery.of(context).size.width,
           height: 50,
           // color: Colors.white54,
           alignment: Alignment.center,
@@ -31,38 +30,40 @@ class AppBarWidget extends StatelessWidget with PreferredSizeWidget {
             controller: _seachController,
             textAlign: TextAlign.justify,
             decoration: InputDecoration(
-                hintText: S
-                    .of(context)
-                    .seach_product,
+                hintText: S.of(context).seach_product,
                 prefixIcon: Icon(Icons.search),
                 enabledBorder: OutlineInputBorder(
-                    borderSide:
-                    BorderSide(width: 5, color: Colors.transparent),
+                    borderSide: BorderSide(width: 5, color: Colors.transparent),
                     borderRadius: BorderRadius.all(Radius.circular(10.0))),
                 focusedBorder: OutlineInputBorder(
-                    borderSide:
-                    BorderSide(width: 5, color: Colors.transparent),
+                    borderSide: BorderSide(width: 5, color: Colors.transparent),
                     borderRadius: BorderRadius.all(Radius.circular(10.0))),
                 border: OutlineInputBorder(
-                    borderSide:
-                    BorderSide(width: 5, color: Colors.transparent),
+                    borderSide: BorderSide(width: 5, color: Colors.transparent),
                     borderRadius: BorderRadius.all(Radius.circular(10.0)))),
             onChanged: (value) {
-              _controller.catalogs.value = _controller.catalogs.value
-                  .where((element) {
-                element.catalogs!.where((element1) {
-                  return element1.catalogname!
-                      .toLowerCase()
-                      .contains(_controller.search.value.toLowerCase());
-                }).toList();
-                return element.catalogname!
-                    .toLowerCase()
-                    .contains(_controller.search.value.toLowerCase());
-              })
-                  .toList();
-              _controller.catalogs.refresh();
-            },),
-
+              if (value != '') {
+                getCatalogs(value, _controller.catalogs.value);
+                _controller.catalogs.value = finalList;
+                _controller.catalogs.refresh();
+              } else {
+                _controller.fetchGetAll();
+              }
+            },
+          ),
         ));
+  }
+
+  getCatalogs(String value, List<Catalog> list) {
+    finalList = [];
+
+    list.forEach((element) {
+      if (element.catalogname!.toLowerCase().contains(value.toLowerCase())) {
+        finalList.add(element);
+      }
+      if (element.catalogs!.isNotEmpty) {
+        getCatalogs(value, element.catalogs!);
+      }
+    });
   }
 }
